3b86dd57e636723803e52ec1b2a68ce7e837cf9d
Marvv1ne <gorn89@inbox.ru>
2025-01-05 15:50:02 +0400

    import psycopg2 lib in app.py

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,4 +1,5 @@
 import os
+import psycopg2
 from flask import Flask, render_template
 from dotenv import load_dotenv
 
@@ -6,6 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+DATABASE_URL = os.getenv('DATABASE_URL')
 
 
 @app.route('/')
