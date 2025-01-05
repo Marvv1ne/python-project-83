48147d5a18b3c8677e91345b2490ea1cab90a5a0
Gura-san <gorn89@inbox.ru>
2025-01-05 19:10:41 +0400

    improve flake8 comments

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -9,10 +9,13 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 DATABASE_URL = os.getenv('DATABASE_URL')
 
+conn = psycopg2.connect(DATABASE_URL)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run()
