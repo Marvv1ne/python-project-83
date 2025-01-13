21bba86473b448b6c2b67f4d534b3e3aa8589e8b
Marvv1ne <gorn89@inbox.ru>
2025-01-13 21:43:41 +0400

    move Table class to db.py from db_branch.py

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -1,43 +1,43 @@
-import psycopg2
 from psycopg2.extras import RealDictCursor
 
-
-
-#conn = psycopg2.connect(DATABASE_URL)
-
-
-class UrlsTable:
-    def __init__(self, conn):
+class Table:
+    def __init__(self, conn, table_name):
         self.conn = conn
+        self.table_name = table_name
     
-    def insert(self, name, created_at):
-        sql = "INSERT INTO urls (name, created_at) VALUES (%(name)s, %(created_at)s) RETURNING id;"
-        with self.conn.cursor() as curs:
-            curs.execute(sql, {'name': name, 'created_at': created_at})
-            id = curs.fetchone()[0]
-            self.conn.commit()
-        return id
-    
-    def get_id(self, name):
-        sql = "SELECT id FROM urls WHERE name=%(name)s;"
-        with self.conn.cursor() as curs:
-            curs.execute(sql, {'name': name})
-            id = curs.fetchone()
-            if not id:
-                return None
-        return id[0]
-
-    def get_row(self, id):
-        sql = "SELECT * FROM urls WHERE id=%(id)s;"
-        with self.conn.cursor(cursor_factory=RealDictCursor) as curs:
-            curs.execute(sql, {'id': id})
-            row = curs.fetchone()
-        return row
-    
-    def get_all(self):
-        sql = "SELECT * FROM urls;"
+    def select_all(self):
+        sql = f'SELECT * FROM {self.table_name}'
         with self.conn.cursor(cursor_factory=RealDictCursor) as curs:
             curs.execute(sql)
             table = curs.fetchall()
         return table
     
+    def select_row(self, columns, search):
+        str_cols = ', '.join(columns)
+        column_name, value = list(search.items())[0]        
+        sql = f'SELECT {str_cols} FROM {self.table_name} WHERE {column_name} = %s;'        
+        with self.conn.cursor(cursor_factory=RealDictCursor) as curs:
+            curs.execute(sql, (value, ))
+            row = curs.fetchall()
+        return row
+    
+    def select_element(self, element, search):
+        column_name, value = list(search.items())[0]        
+        sql = f'SELECT {element} FROM {self.table_name} WHERE {column_name} = %s;'        
+        with self.conn.cursor() as curs:
+            curs.execute(sql, (value, ))
+            if curs.fetchone():
+                return curs.fetchone()[0]
+        return None
+    
+    def insert(self, row, returning):
+        columns_names = ', '.join(list(row.keys()))
+        values = tuple(row.values())
+        number_of_columns = ', '.join(['%s' for _ in range(len(values))])
+        sql = f'''INSERT INTO {self.table_name} ({columns_names}) VALUES 
+                 ({number_of_columns}) RETURNING {returning};'''
+        with self.conn.cursor() as curs:
+            curs.execute(sql, values)
+            element = curs.fetchone()[0]
+            self.conn.commit()
+        return element
