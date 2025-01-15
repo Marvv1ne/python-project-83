ed8d16b69581a0d7d14cb451cbfaa0210abeed04
Marvv1ne <gorn89@inbox.ru>
2025-01-15 21:55:57 +0400

    rewrite class in db.py to delet all f-strings

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -1,44 +1,53 @@
 from psycopg2.extras import RealDictCursor, NamedTupleCursor
 
-class Table:
-    def __init__(self, conn, table_name):
+class DataBase:
+    def __init__(self, conn):
         self.conn = conn
-        self.table_name = table_name
     
-    def select_all(self):
-        sql = f'SELECT * FROM {self.table_name}'
-        with self.conn.cursor(cursor_factory=RealDictCursor) as curs:
+    def insert_in_urls(self, name, created_at):
+        sql = 'INSERT INTO urls (name, created_at) VALUES (%s, %s) RETURNING id;'
+        with self.conn.cursor(cursor_factory=NamedTupleCursor) as curs:
+            curs.execute(sql, (name, created_at))
+            self.conn.commit()
+            return curs.fetchone().id
+    
+    def insert_into_url_checks(self, url_info):
+        sql = '''INSERT INTO url_checks (url_id, status_code, h1,
+                                       title, description, created_at)
+                VALUES (%(url_id)s, %(status_code)s, %(h1)s, %(title)s, %(description)s, %(created_at)s)'''
+        #url_info['url_id'] = url_id
+        #url_info['created_at'] = created_at
+        with self.conn.cursor(cursor_factory=NamedTupleCursor) as curs:
+            curs.execute(sql, url_info)
+            self.conn.commit()
+                    
+    def select_all_from_urls(self):
+        sql = f'SELECT * FROM urls'
+        with self.conn.cursor(cursor_factory=NamedTupleCursor) as curs:
             curs.execute(sql)
             table = curs.fetchall()
         return table
     
-    def select_row(self, columns, search):
-        str_cols = ', '.join(columns)
-        column_name, value = list(search.items())[0]        
-        sql = f'SELECT {str_cols} FROM {self.table_name} WHERE {column_name} = %s;'        
-        with self.conn.cursor(cursor_factory=RealDictCursor) as curs:
-            curs.execute(sql, (value, ))
-            row = curs.fetchall()
+    def select_row_from_urls(self, id):
+        sql = 'SELECT * FROM urls WHERE id=%s;'
+        with self.conn.cursor(cursor_factory=NamedTupleCursor) as curs:
+            curs.execute(sql, (id, ))
+            row = curs.fetchone()
         return row
     
-    def select_element(self, element, search):
-        column_name, value = list(search.items())[0]        
-        sql = f'SELECT {element} FROM {self.table_name} WHERE {column_name} = %s;'        
-        with self.conn.cursor(cursor_factory=RealDictCursor) as curs:
-            curs.execute(sql, (value, ))
+    def select_id_from_urls(self, name):
+        sql = 'SELECT id FROM urls WHERE name=%s;'       
+        with self.conn.cursor(cursor_factory=NamedTupleCursor) as curs:
+            curs.execute(sql, (name, ))
             result = curs.fetchone()
-            if not result:
+            if result is None:
                 return None
-            return result[element]
+            return result.id
     
-    def insert(self, row, returning):
-        columns_names = ', '.join(list(row.keys()))
-        values = tuple(row.values())
-        number_of_columns = ', '.join(['%s' for _ in range(len(values))])
-        sql = f'''INSERT INTO {self.table_name} ({columns_names}) VALUES 
-                 ({number_of_columns}) RETURNING {returning};'''
-        with self.conn.cursor(cursor_factory=RealDictCursor) as curs:
-            curs.execute(sql, values)
-            element = curs.fetchone()[returning]
-            self.conn.commit()
-        return element
+    
+    def select_row_from_url_checks(self, url_id):
+        sql = 'SELECT * FROM url_checks WHERE url_id=%s;'
+        with self.conn.cursor(cursor_factory=NamedTupleCursor) as curs:
+            curs.execute(sql, (url_id,))
+            return curs.fetchall()
+    
