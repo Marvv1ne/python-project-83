cf4a4d0d0993103174f78ca922e0cf5532b67b0c
Marvv1ne <gorn89@inbox.ru>
2025-01-16 22:25:34 +0400

    add try-except into insert methods and add connect_db function

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -1,4 +1,10 @@
+import psycopg2
 from psycopg2.extras import RealDictCursor, NamedTupleCursor
+
+
+def connect_to_db(app):
+    return psycopg2.connect(app.config['DATABASE_URL'])
+
 
 class DataBase:
     def __init__(self, conn):
@@ -7,19 +13,25 @@ class DataBase:
     def insert_in_urls(self, name, created_at):
         sql = 'INSERT INTO urls (name, created_at) VALUES (%s, %s) RETURNING id;'
         with self.conn.cursor(cursor_factory=NamedTupleCursor) as curs:
-            curs.execute(sql, (name, created_at))
-            self.conn.commit()
-            return curs.fetchone().id
+            try:
+                curs.execute(sql, (name, created_at))
+                self.conn.commit()
+                return curs.fetchone().id
+            except Exception as e:
+                self.conn.rollback()
+                raise e
     
     def insert_into_url_checks(self, url_info):
         sql = '''INSERT INTO url_checks (url_id, status_code, h1,
                                        title, description, created_at)
                 VALUES (%(url_id)s, %(status_code)s, %(h1)s, %(title)s, %(description)s, %(created_at)s)'''
-        #url_info['url_id'] = url_id
-        #url_info['created_at'] = created_at
         with self.conn.cursor(cursor_factory=NamedTupleCursor) as curs:
-            curs.execute(sql, url_info)
-            self.conn.commit()
+            try:
+                curs.execute(sql, url_info)
+                self.conn.commit()
+            except Exception as e:
+                self.conn.rollback()
+                raise e
                     
     def select_all_from_urls(self):
         sql = f'SELECT * FROM urls'
