d69e59bdf21ad55482ee9fb53d4b1f1a0d38c65d
Marvv1ne <gorn89@inbox.ru>
2025-01-16 22:27:19 +0400

    add request into post_checks function

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,22 +1,22 @@
 import os
-import psycopg2
-from psycopg2.extras import RealDictCursor
-import validators
 import datetime
+import requests
 from flask import Flask, render_template, request, flash, redirect, url_for, get_flashed_messages
-from dotenv import load_dotenv
-from urllib.parse import urlparse
-from .db import DataBase
-from .url_parser import get_info
 
+from .db import DataBase, connect_to_db
+from .url_parser import normalize_url, get_info, validate_url
 
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except ModuleNotFoundError:
+    pass
 
-load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-DATABASE_URL = os.getenv('DATABASE_URL')
+app.config['DATABASE_URL'] = os.getenv('DATABASE_URL')
 
-conn = psycopg2.connect(DATABASE_URL)
+#conn = psycopg2.connect(DATABASE_URL)
 
 
 @app.get('/')
@@ -25,12 +25,14 @@ def index():
 
 @app.post('/')
 def post_new():
+    conn = connect_to_db(app)
     url = request.form.get('url')
-    if not validators.url(url):
-        flash('Некорректный URL', 'error')
+    error = validate_url(url)
+    if error:
+        flash(error, 'danger')
         messages = get_flashed_messages(with_categories=True)
-        return render_template('index.html', messages)
-    normalized_url = f'{urlparse(url).scheme}://{urlparse(url).netloc}'
+        return render_template('index.html', messages=messages)
+    normalized_url = normalize_url(url)
     date = datetime.date.today()
     req = DataBase(conn=conn)
     id = req.select_id_from_urls(normalized_url)
@@ -39,39 +41,43 @@ def post_new():
         return redirect(url_for('get_new', id=id))
     id = req.insert_in_urls(normalized_url, date)
     flash('Страница успешно добавлена', 'success')
+    conn.close()
     return redirect(url_for('get_new', id=id))
 
 @app.route('/urls/<id>')
 def get_new(id):
+    conn = connect_to_db(app)
     req = DataBase(conn=conn)
     url = req.select_row_from_urls(id)
     infos = req.select_row_from_url_checks(id)
     messages = get_flashed_messages(with_categories=True)
+    conn.close()
     return render_template('url.html', url=url, messages=messages, infos=infos, id=id)
 
 @app.route('/urls')
 def get_all():
+    conn = connect_to_db(app)
     req = DataBase(conn=conn)
     urls = req.select_all_from_urls()
+    conn.close()
     return render_template('urls.html', urls=urls)
 
 @app.post('/urls/<id>/checks')
 def post_checks(id):
+    conn = connect_to_db(app)
     req = DataBase(conn=conn)
     url = req.select_row_from_urls(id).name
-    
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+    except requests.RequestException:
+        flash('Произошла ошибка при проверке', 'danger')
+        conn.close()
+        return redirect(url_for('get_new', id=id))
     date = str(datetime.date.today())
     url_info = get_info(url)
     url_info['url_id'] = id
     url_info['created_at'] = date
     req.insert_into_url_checks(url_info)
-    
+    conn.close()
     return redirect(url_for('get_new', id=id))
-
-
-    
-
-    
-
-if __name__ == '__main__':
-    app.run()
