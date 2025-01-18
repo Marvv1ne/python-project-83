aaf15ba8bcc0715999d88b7dbd9e388196d1471a
Gura-san <gorn89@inbox.ru>
2025-01-18 13:23:11 +0400

    rename url_parser.py to utils.py

--- page_analyzer/utils.py
+++ page_analyzer/utils.py
@@ -3,6 +3,7 @@ import requests
 from urllib.parse import urlparse
 from bs4 import BeautifulSoup
 
+
 def validate_url(url):
     if len(url) > 255:
         return 'URL превышает 255 символов'
@@ -11,16 +12,19 @@ def validate_url(url):
     elif not url:
         return 'URL обязателен для заполнения'
 
+
 def normalize_url(url):
     parsed_url = urlparse(url)
     return f'{parsed_url.scheme}://{parsed_url.netloc}'
+
 
 def get_info(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     h1 = soup.find('h1').text[:255] if soup.find('h1') else ''
     title = soup.find('title').text[:255] if soup.find('title') else ''
-    description = soup.find('meta', attrs={'name': 'description'}).get('content', '') if soup.find('meta', attrs={'name': 'description'}) else '';
+    meta = soup.find('meta', attrs={'name': 'description'})
+    description = (meta.get('content', '') if meta else '')
     return {
         'status_code': response.status_code,
         'h1': h1,
