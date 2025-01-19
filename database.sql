7a3550b239b5677bef419f35b42f3957048220cf
Gura-san <gorn89@inbox.ru>
2025-01-19 13:57:40 +0400

    add on delete cascade to foreign key in url_checks table

--- database.sql
+++ database.sql
@@ -9,7 +9,7 @@ CREATE TABLE urls (
 
 CREATE TABLE url_checks (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    url_id BIGINT REFERENCES urls (id),
+    url_id BIGINT REFERENCES urls (id) ON DELETE CASCADE,
     status_code INTEGER,
     h1 VARCHAR(255),
     title VARCHAR(255),
